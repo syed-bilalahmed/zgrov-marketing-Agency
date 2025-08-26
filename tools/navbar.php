624b482c4dc2b50bbec2a08e07e0510742f1dd81
Syed BilalAhmed <syedbilalahmed5292@gmail.com>
2025-08-26 14:22:15 +0500

    set up navbar

---
+++ tools/navbar.php
@@ -0,0 +1,64 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">ZGrov Tools</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+
+        <!-- SEO Tools -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="./seo-tools/index.php" id="seoDropdown" role="button">
+            SEO Tools
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="seoDropdown">
+            <li><a class="dropdown-item" href="#">Keyword Finder</a></li>
+            <li><a class="dropdown-item" href="#">Backlink Checker</a></li>
+            <li><a class="dropdown-item" href="#">Meta Tag Generator</a></li>
+            <li><a class="dropdown-item" href="#">Site Audit</a></li>
+          </ul>
+        </li>
+
+        <!-- Analytics Tools -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="./analytics/" id="analyticsDropdown" role="button">
+            Analytics
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="analyticsDropdown">
+            <li><a class="dropdown-item" href="#">Traffic Analysis</a></li>
+            <li><a class="dropdown-item" href="#">Page Speed Test</a></li>
+            <li><a class="dropdown-item" href="#">Visitor Tracker</a></li>
+          </ul>
+        </li>
+
+        <!-- PDF Tools -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="./pdf-tools/index.php" id="pdfDropdown" role="button">
+            PDF Tools
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="pdfDropdown">
+            <li><a class="dropdown-item" href="#">Merge PDF</a></li>
+            <li><a class="dropdown-item" href="#">Split PDF</a></li>
+            <li><a class="dropdown-item" href="#">PDF to Word</a></li>
+            <li><a class="dropdown-item" href="#">Compress PDF</a></li>
+          </ul>
+        </li>
+
+        <!-- Background Tools -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="./background-tools/index.php" id="bgDropdown" role="button">
+            Background Tools
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="bgDropdown">
+            <li><a class="dropdown-item" href="#">Remove Background</a></li>
+            <li><a class="dropdown-item" href="#">Change Background</a></li>
+            <li><a class="dropdown-item" href="#">Blur Background</a></li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
