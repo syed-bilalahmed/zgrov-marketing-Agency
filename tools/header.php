6cff19e43ac588f7d5ade4e4831058e468da7c4c
Syed BilalAhmed <syedbilalahmed5292@gmail.com>
2025-08-26 14:22:08 +0500

    added header

--- tools/header.php
+++ tools/header.php
@@ -1,1 +1,11 @@
-<a href="./index.php" class="navbar-brand">ZGrov Tools</a>  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ZGrov Tools</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+  
+</head>
